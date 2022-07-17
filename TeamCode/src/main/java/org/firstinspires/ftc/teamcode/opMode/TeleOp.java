5e58d560b91354e90005d0538acd173ed9353ad5
quangngonz <46818455+quangngonz@users.noreply.github.com>
2022-07-17 09:18:55 +0700

    Added subsystems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/TeleOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "QuangNgo")
+public class TeleOp extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
